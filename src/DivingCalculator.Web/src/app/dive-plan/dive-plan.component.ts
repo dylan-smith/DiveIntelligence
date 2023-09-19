aac05f08f30f0c56a3cf554089d12663834c1c21
Dylan Smith <dylan-smith@github.com>
2023-09-19 03:13:17 -0500

    dive-plan-component layout

--- src/DivingCalculator.Web/src/app/dive-plan/dive-plan.component.ts
+++ src/DivingCalculator.Web/src/app/dive-plan/dive-plan.component.ts
@@ -5,4 +5,25 @@ import { Component } from '@angular/core';
   templateUrl: './dive-plan.component.html',
   styleUrls: ['./dive-plan.component.scss'],
 })
-export class DivePlanComponent {}
+export class DivePlanComponent {
+  planEvents = [
+    {
+      Timestamp: '0:00:00',
+      Title: 'Start',
+      Description: 'Air (O<sub>2</sub>: 21%, He: 0%, N<sub>2</sub>: 79%)',
+    },
+    {
+      Timestamp: '0:00:00',
+      Title: 'Surface',
+      Description: 'Ascent time: 0 min @ 10m/min',
+    },
+  ];
+
+  onAddPlanEvent(event: Event) {
+    this.planEvents.push({
+      Timestamp: '1:00',
+      Title: 'New Event',
+      Description: 'New Event Description',
+    });
+  }
+}
