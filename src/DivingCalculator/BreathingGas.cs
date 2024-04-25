b5af8b0f08a72ded91c21f10c7b627aaa3fdbf6a
Dylan Smith <dylan-smith@github.com>
2024-04-25 16:04:36 +0000

    deleted obsolete stuff

--- src/DivingCalculator/BreathingGas.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DivingCalculator
-{
-    public record BreathingGas(double Oxygen, double Nitrogen, double Helium)
-    {
-        public double GetNitrogenPartialPressure(double depth) => ((depth / 10.0) + 1) * Nitrogen;
-        public double GetOxygenPartialPressure(double depth) => ((depth / 10.0) + 1) * Oxygen;
-        public double GetHeliumPartialPressure(double depth) => ((depth / 10.0) + 1) * Helium;
-    }
-}
