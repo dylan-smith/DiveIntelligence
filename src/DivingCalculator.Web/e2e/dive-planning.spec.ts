776983e6afa2cdf90358724d8f3a92b1021d8b9c
Dylan Smith <dylan-smith@github.com>
2024-06-10 20:33:49 -0500

    Implement Page Object Model pattern (#192)
    This pull request includes changes to the end-to-end tests of the
    `DivingCalculator.Web` project. The changes primarily focus on
    refactoring the tests to improve readability and maintainability. The
    tests have been restructured to use Page Object Model (POM), which is a
    design pattern that helps in reducing code duplication and improving
    test maintenance.

--- src/DivingCalculator.Web/e2e/dive-planning.spec.ts
+++ src/DivingCalculator.Web/e2e/dive-planning.spec.ts
@@ -1,4 +1,5 @@
 import { test, expect } from '@playwright/test';
+import { HomePage } from './pages/home.page';
 
 test('shows homepage', async ({ page }) => {
   await page.goto('/');
@@ -6,51 +7,53 @@ test('shows homepage', async ({ page }) => {
 });
 
 test('NDL dive to 25m for 50 mins on nitrox 32', async ({ page }) => {
-  await page.goto('/');
-  await page.getByRole('button', { name: 'Plan a Dive' }).click();
+  const homePage = await new HomePage(page).goto();
 
-  await page.getByText('Nitrox 32').click();
-  await expect(page.locator('dive-start-gas-stats .gas-calculations .gas-calc').getByText('Max Depth (PO2): 33m (40m deco)')).toBeVisible();
-  await expect(page.locator('dive-start-gas-stats .gas-calculations .gas-calc').getByText('Max Depth (END): 30m')).toBeVisible();
-  await expect(page.locator('dive-start-gas-stats .gas-calculations .gas-calc').getByText('Min Depth (Hypoxia): 0m')).toBeVisible();
-  await page.getByRole('button', { name: 'Save' }).click();
+  const newDivePage = await homePage.planADive();
 
-  await page.locator('button').filter({ hasText: 'add' }).click();
+  await newDivePage.selectStandardGas('Nitrox 32');
+  expect(await newDivePage.getMaxDepthPO2()).toBe('33m (40m deco)');
+  expect(await newDivePage.getMaxDepthEND()).toBe('30m');
+  expect(await newDivePage.getMinDepthHypoxia()).toBe('0m');
 
-  await expect(page.locator('dive-current-stats .current-stats .dive-stat').getByText('Current Depth: 0m')).toBeVisible();
-  await expect(page.locator('dive-current-stats .current-stats .dive-stat').getByText('Current Ceiling: 0m')).toBeVisible();
-  await expect(page.locator('dive-current-stats .current-stats .dive-stat').getByText('Current Gas: Nitrox 32 (O2: 32%, He: 0%, N2: 68%)')).toBeVisible();
-  await expect(page.locator('dive-current-stats .current-stats .dive-stat').getByText('Max Depth (PO2): 33m (40m deco)')).toBeVisible();
-  await expect(page.locator('dive-current-stats .current-stats .dive-stat').getByText('Max Depth (END): 30m')).toBeVisible();
-  await expect(page.locator('dive-current-stats .current-stats .dive-stat').getByText('Min Depth (Hypoxia): 0m')).toBeVisible();
-  await expect(page.locator('dive-current-stats .current-stats .dive-stat').getByText('PO2: 0.32')).toBeVisible();
-  await expect(page.locator('dive-current-stats .current-stats .dive-stat').getByText('END: 0m')).toBeVisible();
+  let diveOverviewPage = await newDivePage.Save();
+  const addDiveSegmentPage = await diveOverviewPage.addSegment();
+  expect(await addDiveSegmentPage.getCurrentDepth()).toBe('0m');
+  expect(await addDiveSegmentPage.getCurrentCeiling()).toBe('0m');
+  expect(await addDiveSegmentPage.getCurrentGas()).toBe('Nitrox 32 (O2: 32%, He: 0%, N2: 68%)');
+  expect(await addDiveSegmentPage.getCurrentMaxDepthPO2()).toBe('33m (40m deco)');
+  expect(await addDiveSegmentPage.getCurrentMaxDepthEND()).toBe('30m');
+  expect(await addDiveSegmentPage.getCurrentMinDepthHypoxia()).toBe('0m');
+  expect(await addDiveSegmentPage.getCurrentPO2()).toBe('0.32');
+  expect(await addDiveSegmentPage.getCurrentEND()).toBe('0m');
 
-  await page.getByLabel('New Depth (m)').fill('25');
-  await expect(page.locator('dive-new-depth-stats .new-depth-stats .dive-stat').getByText('Descent Time: 1 min 15 sec @ 20m/min')).toBeVisible();
-  await expect(page.locator('dive-new-depth-stats .new-depth-stats .dive-stat').getByText('PO2: 1.12')).toBeVisible();
-  await expect(page.locator('dive-new-depth-stats .new-depth-stats .dive-stat').getByText('END: 26m')).toBeVisible();
+  await addDiveSegmentPage.setNewDepth(25);
+  expect(await addDiveSegmentPage.getDescentTime()).toBe('1 min 15 sec @ 20m/min');
+  expect(await addDiveSegmentPage.getNewDepthPO2()).toBe('1.12');
+  expect(await addDiveSegmentPage.getNewDepthEND()).toBe('26m');
 
-  await expect(page.locator('dive-new-gas-stats .new-gas-stats .gas-calculation-chunk .dive-stat').getByText('PO2: 1.12')).toBeVisible();
-  await expect(page.locator('dive-new-gas-stats .new-gas-stats .gas-calculation-chunk .dive-stat').getByText('END: 26m')).toBeVisible();
-  await expect(page.locator('dive-new-gas-stats .new-gas-stats .gas-calculation-chunk .dive-stat').getByText('No Deco Limit: 57 min 4 sec')).toBeVisible();
-  await expect(page.locator('dive-new-gas-stats .new-gas-stats .gas-calculation-chunk .dive-stat').getByText('Max Depth (PO2): 33m (40m deco)')).toBeVisible();
-  await expect(page.locator('dive-new-gas-stats .new-gas-stats .gas-calculation-chunk .dive-stat').getByText('Max Depth (END): 30m')).toBeVisible();
-  await expect(page.locator('dive-new-gas-stats .new-gas-stats .gas-calculation-chunk .dive-stat').getByText('Min Depth (Hypoxia): 0m')).toBeVisible();
+  expect(await addDiveSegmentPage.getNewGasPO2()).toBe('1.12');
+  expect(await addDiveSegmentPage.getNewGasEND()).toBe('26m');
+  expect(await addDiveSegmentPage.getNewGasNoDecoLimit()).toBe('57 min 4 sec');
+  expect(await addDiveSegmentPage.getNewGasMaxDepthPO2()).toBe('33m (40m deco)');
+  expect(await addDiveSegmentPage.getNewGasMaxDepthEND()).toBe('30m');
+  expect(await addDiveSegmentPage.getNewGasMinDepthHypoxia()).toBe('0m');
 
-  await page.getByLabel('Time at Depth (mins)').fill('50');
-  await expect(page.locator('dive-new-time-stats .new-time-stats .dive-stat').getByText('Ceiling: 0m')).toBeVisible();
-  await expect(page.locator('dive-new-time-stats .new-time-stats .dive-stat').getByText('Total Dive Duration: 51 min 15 sec')).toBeVisible();
+  await addDiveSegmentPage.setTimeAtDepth(50);
+  expect(await addDiveSegmentPage.getFinalCeiling()).toBe('0m');
+  expect(await addDiveSegmentPage.getTotalDiveDuration()).toBe('51 min 15 sec');
 
-  await page.getByRole('button', { name: 'Save' }).click();
-  await expect(page.getByRole('listbox').locator('mat-list-item').first()).toContainText('scuba_diving 0:00 Start Dive');
-  await expect(page.getByRole('listbox').locator('mat-list-item').first()).toContainText('Nitrox 32 (O2: 32%, He: 0%, N2: 68%)');
-  await expect(page.getByRole('listbox').locator('mat-list-item').nth(1)).toContainText('arrow_downward 0:00 Descend to 25m');
-  await expect(page.getByRole('listbox').locator('mat-list-item').nth(1)).toContainText('Descent time: 1 min 15 sec @ 20m/min');
-  await expect(page.getByRole('listbox').locator('mat-list-item').nth(2)).toContainText('done 51:15 Surface');
-  await expect(page.getByRole('listbox').locator('mat-list-item').nth(2)).toContainText('Ascent time: 2 min 30 sec @ 10m/min');
+  diveOverviewPage = await addDiveSegmentPage.Save();
 
-  await expect(page.locator('dive-dive-summary .dive-stats .dive-stat').getByText('Dive Duration: 53 min 45 sec')).toBeVisible();
-  await expect(page.locator('dive-dive-summary .dive-stats .dive-stat').getByText('Max Depth: 25m')).toBeVisible();
-  await expect(page.locator('dive-dive-summary .dive-stats .dive-stat').getByText('Average Depth: 24m')).toBeVisible();
+  const diveSegments = await diveOverviewPage.getDiveSegments();
+  expect(diveSegments[0].heading).toBe('scuba_diving 0:00 Start Dive');
+  expect(diveSegments[0].details).toBe('Nitrox 32 (O2: 32%, He: 0%, N2: 68%)');
+  expect(diveSegments[1].heading).toBe('arrow_downward 0:00 Descend to 25m');
+  expect(diveSegments[1].details).toBe('Descent time: 1 min 15 sec @ 20m/min');
+  expect(diveSegments[2].heading).toBe('done 51:15 Surface');
+  expect(diveSegments[2].details).toBe('Ascent time: 2 min 30 sec @ 10m/min');
+
+  expect(await diveOverviewPage.getDiveDuration()).toBe('53 min 45 sec');
+  expect(await diveOverviewPage.getMaxDepth()).toBe('25m');
+  expect(await diveOverviewPage.getAverageDepth()).toBe('24m');
 });
